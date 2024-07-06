15594873acf55105f423ee4ccc3010685aeee326
kei615 <from0615.ykhm@gmail.com>
2024-07-06 19:33:49 +0900

    typeof演算子を使って型を確認する

---
+++ section1/main.js
@@ -0,0 +1,16 @@
+// 変数とデータ型　型の確認方法
+
+// typeof演算子とは、データ型を返す演算子
+// typeof 演算子の後に変数や値を指定すると、そのデータ型を返す
+// tデータ型を返すので、データ型を確認する際に使用する
+
+let num = 42;
+let str = 'Hello';
+let bool = true;
+
+console.log(typeof num); // number
+console.log(typeof str); // string
+console.log(typeof bool); // boolean
+
+// 変数の型を確認する目的は、データ型によって処理を分岐させるため
+// if文を使って、データ型によって処理を分岐させることができる
