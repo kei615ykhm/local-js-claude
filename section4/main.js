c605fefd95dd284f76ff5a01904d73ab1c0d4695
kei615 <from0615.ykhm@gmail.com>
2024-07-08 21:31:59 +0900

    for...inの繰り返し処理
    - 具体的な使い方
    - データの表示
    - データの変換
    - データの集計
    - フィルタリング処理

--- section4/main.js
+++ section4/main.js
@@ -64,3 +64,39 @@ for (let key in person) {
 for (let key in object) {
   // オブジェクトのプロパティに対する処理
 }
+
+// 具体的な使い方
+
+// データの表示
+
+let person = { name: '太郎', age: 30, city: '東京' };
+for (let key in person) {
+  console.log(key + ': ' + person[key]);
+}
+
+// データの変換
+
+let prices = { apple: 100, banana: 80, orange: 120 };
+for (let item in prices) {
+  prices[item] *= 1.1; // 10%値上げ
+}
+
+// データの集計
+
+let sales = { monday: 150, tuesday: 200, wednesday: 180 };
+let total = 0;
+for (let day in sales) {
+  total += sales[day];
+}
+console.log('Total sales: ' + total);
+
+// 特定の条件に基づくフィルタリング処理
+
+let inventory = { apples: 5, bananas: 0, oranges: 3 };
+let inStock = [];
+for (let fruit in inventory) {
+  if (inventory[fruit] > 0) {
+    inStock.push(fruit);
+  }
+}
+console.log('In stock: ' + inStock.join(', '));
