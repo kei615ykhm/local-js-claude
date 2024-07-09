5fa5fcec56313fba5307b561dfd0158891b0cd06
kei615 <from0615.ykhm@gmail.com>
2024-07-09 21:56:55 +0900

    クラス
    - 使用

--- section5/main.js
+++ section5/main.js
@@ -10,3 +10,8 @@ class Car {
     return `This car is a ${this.brand} ${this.model}.`;
   }
 }
+
+// クラスの使用
+
+let myCar = new Car('Toyota', 'Corolla');
+console.log(myCar.describe()); // 出力: "This car is a Toyota Corolla."
