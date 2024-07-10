5f62ae307ca0859a6a62b27bad6662675e023103
kei615 <from0615.ykhm@gmail.com>
2024-07-10 23:52:50 +0900

    chore: eslint導入に関する設定
    - eslintをes12対応に設定した
    - settings.jsonにてコマンド実行時に明示的な修正を実行する設定にした

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+};
