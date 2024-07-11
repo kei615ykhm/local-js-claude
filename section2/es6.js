d3fd8a025f4af3e230d28a1882fbd518bba9bbb5
kei615 <from0615.ykhm@gmail.com>
2024-07-11 15:33:12 +0900

    アロー関数
    - functionキーワード関数式との違い
    - 基本的なアロー関数式と省略記法

--- section2/es6.js
+++ section2/es6.js
@@ -97,3 +97,24 @@ const fuu = {
 fuu.baz(); // I am `baz` method
 
 // 結果は同じになる
+
+// アロー関数
+
+// function キーワードを使った物のほかに、アロー関数という関数の書き方がある（ES2015:ES5以降）
+
+// function キーワードによる関数式
+const plusOne = function (n) {
+  return n + 1;
+};
+
+// アロー関数式
+const addOne = (n) => {
+  return n + 1;
+};
+
+// アロー関数式、さらに省略記法
+const increment = (n) => n + 1;
+
+console.log(plusOne(4)); // 5
+console.log(addOne(4)); // 5
+console.log(increment(4)); // 5
