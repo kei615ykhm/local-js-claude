372510d64bfa3eb58e37a193c29255d495787ce4
kei615 <from0615.ykhm@gmail.com>
2024-07-13 18:18:27 +0900

    分割代入の簡単な例
    - 配列の分割代入の例
    - 通常の代入、分割代入、結果、簡略化

--- section4/es6.js
+++ section4/es6.js
@@ -23,3 +23,22 @@ console.log(obj1); // { foo: 8, '<fuu>': 16, key: 128, _key2: 256, baz: 512 }
 // 分割代入の簡略化
 const obj2 = { baz };
 console.log(obj2); // { baz: 1024 }
+
+// // 配列の分割代入
+
+// 通常の代入
+const [a, b] = ['foo', 'bar'];
+console.log(a, b); // foo bar
+
+// 分割代入
+const [, n] = [1, 4];
+console.log(n); // 4
+
+// 分割代入の結果
+const [, , i, , j, , , k] = [1, 2, 3, 4, 5, 6, 7];
+console.log(i, j, k); // 3 5 undefined
+
+// 分割代入の簡略化
+const profile = { name: 'Kanae', age: 24, gender: 'f' };
+const { name, age } = profile;
+console.log(name, age); // Kanae 24
